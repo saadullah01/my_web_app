57adf90af90bae318c565276d78e0cbc06e94cc9
saadullah01 <saadullah1912@gmail.com>
2020-12-28 08:28:15 +0000

    added Firebase Remote Config

--- services/web/src/client.js
+++ services/web/src/client.js
@@ -1,5 +1,6 @@
 import * as sapper from '@sapper/app';
-
+import { initFeatureToggles } from "./feature-toggles";
+initFeatureToggles();
 sapper.start({
 	target: document.querySelector('#sapper')
 });

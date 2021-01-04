5d7ed88985acb2b70092f894a3f27ab398e157dc
saadullah01 <saadullah1912@gmail.com>
2021-01-04 11:57:34 +0000

    Added test to check 5 blog posts

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -1,3 +1,4 @@
+const posts = require('../../src/routes/blog/_posts').default;
 describe('Blog posts', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
@@ -5,5 +6,13 @@ describe('Blog posts', () => {
 
   it('has the correct <h1>', () => {
     cy.contains('h1', 'Great success!')
+  });
+
+  posts.forEach(post => {
+    it(`lists the "${post.title}" blog post`, () => {
+      cy
+        .contains('[data-cy=blog-posts-list] li a', post.title)
+        .should('have.attr', 'href', `blog/${post.slug}`)
+    })
   });
 });

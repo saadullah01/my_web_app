144ba829f3a7037b945012ee180539eac6abd89b
saadullah01 <saadullah1912@gmail.com>
2021-01-04 12:47:25 +0000

    Added test to check 5 blog posts

---
+++ services/web/cypress/integration/blogs-test.spec.js
@@ -0,0 +1,14 @@
+const posts = require('../../src/routes/blog/_posts');
+describe('Blog posts', () => {
+  beforeEach(() => {
+   cy.visit('http://localhost:3000/blog')
+  });
+
+  posts.forEach(post => {
+    it(`lists the "${post.title}" blog post`, () => {
+      cy
+        .contains('[data-cy=blog-posts-list] li a', post.title)
+        .should('have.attr', 'href', `blog/${post.slug}`)
+    })
+  });
+});
--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -1,4 +1,3 @@
-const posts = require('../../src/routes/blog/_posts').default;
 describe('Blog posts', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
@@ -6,13 +5,5 @@ describe('Blog posts', () => {
 
   it('has the correct <h1>', () => {
     cy.contains('h1', 'Great success!')
-  });
-
-  posts.forEach(post => {
-    it(`lists the "${post.title}" blog post`, () => {
-      cy
-        .contains('[data-cy=blog-posts-list] li a', post.title)
-        .should('have.attr', 'href', `blog/${post.slug}`)
-    })
   });
 });

3d3bbc228fee944ad03237b7b47d345d79b9a5e6
saadullah01 <saadullah1912@gmail.com>
2021-01-04 11:50:10 +0000

    added test to check correct <h1> tag

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -1,1 +1,9 @@
+describe('Blog posts', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
 
+  it('has the correct <h1>', () => {
+    cy.contains('h1', 'Great success!')
+  });
+});

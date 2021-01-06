e5e86d1cf40f774e1182764b4afbc1e5288bfa5f
saadullah01 <saadullah1912@gmail.com>
2021-01-06 14:37:24 +0000

    added test using Testing Library

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -1,9 +1,19 @@
-describe('Blog posts', () => {
+describe('Heading', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   });
 
   it('has the correct <h1>', () => {
     cy.contains('h1', 'Great success!')
+  });
+});
+
+describe("Sapper template app", () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+
+  it("has the correct heading", () => {
+   cy.findByRole("heading").should("contain", "Great success!");
   });
 });

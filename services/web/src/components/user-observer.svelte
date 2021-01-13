30aff9508360abf512ac1c284129c2abb64c6869
saadullah01 <saadullah1912@gmail.com>
2021-01-13 10:05:09 +0000

    Redirect once authenticated

--- services/web/src/components/user-observer.svelte
+++ services/web/src/components/user-observer.svelte
@@ -1,10 +1,19 @@
 <script>
   import { onMount } from "svelte";
+  import { goto, stores } from "@sapper/app";
   import {currentUser} from "../stores/user";
 
+  const { page } = stores();
   onMount(() => {
     firebase.auth().onAuthStateChanged(function(user) {
-      user ? currentUser.signIn(user) : currentUser.signOut();
+      if (user) {
+      currentUser.signIn(user);
+      if ($page.query.redirect) {
+        goto($page.query.redirect);
+      }
+    } else {
+      currentUser.signOut();
+    }
     });
   });
 </script>

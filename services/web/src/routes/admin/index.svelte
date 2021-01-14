18e0556002469e3b53fe8e03e8472f7a2a5c85a2
saadullah01 <saadullah1912@gmail.com>
2021-01-14 12:51:05 +0000

    Create user interface to write data

--- services/web/src/routes/admin/index.svelte
+++ services/web/src/routes/admin/index.svelte
@@ -2,6 +2,7 @@
   import { goto } from '@sapper/app';
   import { onMount } from "svelte";
   import { currentUser } from "../../stores/user";
+  import NewBlogPost from "../../components/new-blog-post.svelte";
 
   onMount(() => {
     if (!$currentUser) {
@@ -13,4 +14,5 @@
 {#if $currentUser}
   <h1>Admin dashboard</h1>
   <p>Only authenticated users have access to this.</p>
+  <NewBlogPost />
 {/if}

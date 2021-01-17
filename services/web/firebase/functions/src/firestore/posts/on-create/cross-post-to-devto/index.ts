851578e6c0422576962a5a03fad0bd212601cdb7
saadullah01 <saadullah1912@gmail.com>
2021-01-17 15:12:07 +0000

    Develop the New Service Placeholder

---
+++ services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto/index.ts
@@ -0,0 +1,8 @@
+import * as functions from "firebase-functions";
+
+export default functions.firestore
+  .document('posts/{slug}')
+  .onCreate((snapshot: functions.firestore.DocumentSnapshot,
+context: functions.EventContext) => {
+  console.log(`New post created with slug: ${context.params.slug}. Post: ${JSON.stringify(snapshot.data())}`)
+});

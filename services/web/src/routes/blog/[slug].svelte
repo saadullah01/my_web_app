5dd0d93548f3ab1113201d6ffffa200b7f8af8b8
saadullah01 <saadullah1912@gmail.com>
2021-06-10 08:02:55 +0000

    Convert Markdown to HTML

--- services/web/src/routes/blog/[slug].svelte
+++ services/web/src/routes/blog/[slug].svelte
@@ -2,7 +2,7 @@
 	export async function preload({ params }) {
 		if (typeof window !== "undefined") {
 			const doc = await
-			window.db.collection("posts").doc(params.slug).get();
+			window.db.collection("posts").doc( params.slug).get();
 			if (doc.exists) {
 				return { post: doc.data() }
 			} else {
@@ -13,8 +13,8 @@
 </script>
 
 <script>
-	export let post;
 	import marked from "marked";
+	export let post;
 </script>
 
 <style>
@@ -30,6 +30,7 @@
 		font-size: 1.4em;
 		font-weight: 500;
 	}
+
 	.content :global(pre) {
 		background-color: #f9f9f9;
 		box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
@@ -37,20 +38,23 @@
 		border-radius: 2px;
 		overflow-x: auto;
 	}
+
 	.content :global(pre) :global(code) {
 		background-color: transparent;
 		padding: 0;
 	}
+
 	.content :global(ul) {
 		line-height: 1.5;
 	}
+
 	.content :global(li) {
 		margin: 0 0 0.5em 0;
 	}
 </style>
 
 <svelte:head>
-	<title>{post ? post.title : ''}</title>
+  <title>{post ? post.title : ''}</title>
 </svelte:head>
 
 {#if post}

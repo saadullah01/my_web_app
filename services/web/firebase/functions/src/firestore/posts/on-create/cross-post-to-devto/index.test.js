0e0008adb0e47785057986c6cba805bdb2df9e9d
saadullah01 <saadullah1912@gmail.com>
2021-06-10 08:11:50 +0000

    Write a test for the service

--- services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto/index.test.js
+++ services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto/index.test.js
@@ -5,8 +5,8 @@ const test = require('firebase-functions-test')();
 
 test.mockConfig({
     crossposttodevto: {
-        apikey: 'test-api-key'
-    }
+      apikey: 'test-api-key'
+  }
 });
 
 describe("cross-post-to-devto", () => {
@@ -14,45 +14,46 @@ describe("cross-post-to-devto", () => {
     let consoleErrorStub;
 
     before(() => {
-        testFunctions = require('../../../../../lib');
+    testFunctions = require('../../../../../lib');
     });
 
     after(() => {
-        consoleErrorStub.restore();
-        test.cleanup();
+    consoleErrorStub.restore();
+    test.cleanup();
     });
 
     it("calls the DEV API with correct parameters", async () => {
-        const gotPostSpy = sinon.spy(got, 'post');
-        consoleErrorStub = sinon.stub(console, 'error');
 
-        const wrapped = test.wrap(testFunctions.firestore.posts.onCreate.crossPostToDevto);
-    
-        const snapshot = {
-            data: () => ({
-                title: 'Test Title',
-                content: 'Test content'
-            })
-        };
+      const gotPostSpy = sinon.spy(got, 'post');
+      consoleErrorStub = sinon.stub(console, 'error');
 
-        await wrapped(snapshot, {
-            params: {
-                slug: 'test-title'
-            }
-        });
+      const wrapped = test.wrap(testFunctions.firestore.posts.onCreate.crossPostToDevto);
 
-        expect(gotPostSpy.calledOnce);
-        expect(gotPostSpy.lastCall.firstArg).to.equal("https://dev.to/api/articles");
-        expect(gotPostSpy.lastCall.lastArg).to.deep.equal({
-            headers: { 'api-key': 'test-api-key' },
-            json: {
-                article: {
-                    title: 'Test Title',
-                    body_markdown: 'Test content',
-                    published: false
-                }
-            }
-        });
-        expect(consoleErrorStub.lastCall.firstArg.message).to.have.string("401 (Unauthorized)");
-    });
+      const snapshot = {
+        data: () => ({
+          title: 'Test Title',
+          content: 'Test content'
+        })
+      };
+
+      await wrapped(snapshot, {
+        params: {
+          slug: 'test-title'
+        }
+      });
+
+      expect(gotPostSpy.calledOnce);
+      expect(gotPostSpy.lastCall.firstArg).to.equal(" https://dev.to/api/articles ");
+      expect(gotPostSpy.lastCall.lastArg).to.deep.equal({
+        headers: { 'api-key': 'test-api-key' },
+        json: {
+          article: {
+            title: 'Test Title',
+            body_markdown: 'Test content',
+            published: false
+          }
+        }
+      });
+      expect(consoleErrorStub.lastCall.firstArg.message).to.have.string("401 (Unauthorized)");
+  });
 });
